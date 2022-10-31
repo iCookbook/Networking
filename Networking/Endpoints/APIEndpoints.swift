5eb552fe8728aabb86139ff6d21eda54c4d6ae21
Egor Badmaev <60363270+htmlprogrammist@users.noreply.github.com>
2022-10-31 11:59:23 +0300

    feat: implement some endpoints

--- Networking/Endpoints/APIEndpoints.swift
+++ Networking/Endpoints/APIEndpoints.swift
@@ -5,11 +5,38 @@
 //  Created by Егор Бадмаев on 30.10.2022.
 //
 
+import Models
+
 /// Here you can see endpoints for current API - _"Edamam API"_
 extension Endpoint {
-    public static func mostactive() -> Self {
-        return Endpoint(path: "stable/stock/market/list/mostactive",
-                        paratemets: ["token": apiKey]
+    public static func random() -> Self {
+        return Endpoint(path: "api/recipes/v2",
+                        paratemets: ["type": "public",
+                                     "app_id": appId,
+                                     "app_key": apiKey,
+                                     "q": keywords.randomElement() ?? "chichen"]
+        )
+    }
+    
+    public static func create(by keyword: String) -> Self {
+        return Endpoint(path: "api/recipes/v2",
+                        paratemets: ["type": "public",
+                                     "app_id": appId,
+                                     "app_key": apiKey,
+                                     "q": keyword]
+        )
+    }
+    
+    public static func create(by keyword: String, with filter: Recipe) -> Self {
+        return Endpoint(path: "api/recipes/v2",
+                        paratemets: ["type": "public",
+                                     "app_id": appId,
+                                     "app_key": apiKey,
+                                     "q": keyword,
+                                     "diet": "high-fiber",
+                                     "cuisineType": "American",
+                                     "mealType": "Dinner",
+                                     "dishType": "Biscuits%20and%20cookies"]
         )
     }
 }
