7fe65dea9abc559a4035d567be1ce4e59e7b1a92
Egor Badmaev <60363270+htmlprogrammist@users.noreply.github.com>
2022-11-01 23:21:25 +0300

    refactor: add comments to the API endpoints

--- Networking/Endpoints/APIEndpoints.swift
+++ Networking/Endpoints/APIEndpoints.swift
@@ -9,6 +9,9 @@ import Models
 
 /// Here you can see endpoints for current API - _"Edamam API"_
 extension Endpoint {
+    /// Endpoint for 20 random recipes.
+    /// Random is done on random choosing keyword that is required for this API. So, it is not impossible to get true random recipes.
+    /// - Returns: `Endpoint` instance
     public static func random() -> Self {
         return Endpoint(path: "api/recipes/v2",
                         paratemets: ["type": "public",
@@ -18,6 +21,9 @@ extension Endpoint {
         )
     }
     
+    /// Creates endpoint with provided keyword
+    /// - Parameter keyword: keyword to find (required)
+    /// - Returns: `Endpoint` instance
     public static func create(by keyword: String) -> Self {
         return Endpoint(path: "api/recipes/v2",
                         paratemets: ["type": "public",
@@ -27,7 +33,15 @@ extension Endpoint {
         )
     }
     
-    public static func create(by keyword: String, with filter: Recipe) -> Self {
+    /// Creates endpoint with provided keyword and **filters**
+    /// - Parameters:
+    ///   - keyword: keyword to find (required)
+    ///   - meals: meal types
+    ///   - diets: diet types
+    ///   - cuisines: world cuisines
+    ///   - dishes: dish types
+    /// - Returns: `Endpoint` instance
+    public static func create(by keyword: String, meals: [Meal] = [], diets: [Diet] = [], cuisines: [Cuisine] = [], dishes: [Dish] = []) -> Self {
         return Endpoint(path: "api/recipes/v2",
                         paratemets: ["type": "public",
                                      "app_id": appId,
